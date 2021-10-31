2a5899b27391b1759c1ec782263bc1785ee84f81
Yu Tao <yutaolife@users.noreply.github.com>
2021-10-31 18:40:43 +0800

    Update run_squad.py
    modify examples

--- examples/run_squad.py
+++ examples/run_squad.py
@@ -116,81 +116,72 @@ class InputFeatures(object):
 
 
 def read_squad_examples(input_file, is_training, version_2_with_negative):
-    """Read a SQuAD json file into a list of SquadExample."""
-    with open(input_file, "r", encoding='utf-8') as reader:
-        input_data = json.load(reader)["data"]
-
-    def is_whitespace(c):
-        if c == " " or c == "\t" or c == "\r" or c == "\n" or ord(c) == 0x202F:
-            return True
-        return False
-
-    examples = []
-    for entry in input_data:
-        for paragraph in entry["paragraphs"]:
-            paragraph_text = paragraph["context"]
-            doc_tokens = []
-            char_to_word_offset = []
-            prev_is_whitespace = True
-            for c in paragraph_text:
-                if is_whitespace(c):
-                    prev_is_whitespace = True
-                else:
-                    if prev_is_whitespace:
-                        doc_tokens.append(c)
-                    else:
-                        doc_tokens[-1] += c
-                    prev_is_whitespace = False
-                char_to_word_offset.append(len(doc_tokens) - 1)
-
-            for qa in paragraph["qas"]:
-                qas_id = qa["id"]
-                question_text = qa["question"]
-                start_position = None
-                end_position = None
-                orig_answer_text = None
-                is_impossible = False
-                if is_training:
-                    if version_2_with_negative:
-                        is_impossible = qa["is_impossible"]
-                    if (len(qa["answers"]) != 1) and (not is_impossible):
-                        raise ValueError(
-                            "For training, each question should have exactly 1 answer.")
-                    if not is_impossible:
-                        answer = qa["answers"][0]
-                        orig_answer_text = answer["text"]
-                        answer_offset = answer["answer_start"]
-                        answer_length = len(orig_answer_text)
-                        start_position = char_to_word_offset[answer_offset]
-                        end_position = char_to_word_offset[answer_offset + answer_length - 1]
-                        # Only add answers where the text can be exactly recovered from the
-                        # document. If this CAN'T happen it's likely due to weird Unicode
-                        # stuff so we will just skip the example.
-                        #
-                        # Note that this means for training mode, every example is NOT
-                        # guaranteed to be preserved.
-                        actual_text = " ".join(doc_tokens[start_position:(end_position + 1)])
-                        cleaned_answer_text = " ".join(
-                            whitespace_tokenize(orig_answer_text))
-                        if actual_text.find(cleaned_answer_text) == -1:
-                            logger.warning("Could not find answer: '%s' vs. '%s'",
-                                           actual_text, cleaned_answer_text)
-                            continue
-                    else:
-                        start_position = -1
-                        end_position = -1
-                        orig_answer_text = ""
-
+  """Read a SQuAD json file into a list of SquadExample."""
+  examples = []
+  doc_tokens = []
+  with open(input_file,"r",encoding="utf-8") as f:
+      js_str = f.read()
+  train_dict = json.loads(js_str)
+  data = train_dict['data'][0]
+  paragraphs = data['paragraphs']
+  #print(" ".join(doc_tokens))
+  #prev_is_whitespace = True
+  for paragraph in paragraphs:
+      paragraph_text = paragraph['context']
+      for i in range(0,len(paragraph_text)):
+          doc_tokens.append(paragraph_text[i])
+      #print(doc_tokens)
+      qas = paragraph['qas']
+      if is_training:
+        for qa in qas:
+            qa_id = qa['id']
+            question = qa['question']
+            start_position = None
+            end_position = None
+            orig_answer_text = None
+            is_impossible = False
+            #print(question)
+            answers = qa['answers']
+            for answer in answers:
+                #print(answer['text'])
+                answer_offset = answer["answer_start"]
+                #print("answer_offset: " + str(answer_offset))
+                orig_answer_text = answer['text']
+                answer_length = len(orig_answer_text)
+                #print("answer_length: " + str(answer_length))
+                start_position = answer_offset
+                #print("start_position: " +  str(start_position))
+                end_position = answer_offset + answer_length - 1
+                #print("end_position: " + str(end_position))
                 example = SquadExample(
-                    qas_id=qas_id,
-                    question_text=question_text,
+                    qas_id=qa_id,
+                    question_text=question,
                     doc_tokens=doc_tokens,
                     orig_answer_text=orig_answer_text,
                     start_position=start_position,
                     end_position=end_position,
                     is_impossible=is_impossible)
                 examples.append(example)
-    return examples
+      else:
+        for qa in qas:
+            qa_id = qa['id']
+            question = qa['question']
+            start_position = None
+            end_position = None
+            orig_answer_text = None
+            is_impossible = False
+            example = SquadExample(
+                      qas_id=qa_id,
+                      question_text=question,
+                      doc_tokens=doc_tokens,
+                      orig_answer_text=orig_answer_text,
+                      start_position=start_position,
+                      end_position=end_position,
+                      is_impossible=is_impossible)
+            examples.append(example)
+
+  return examples
+    
 
 
 def convert_examples_to_features(examples, tokenizer, max_seq_length,
@@ -617,15 +608,15 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
                 all_predictions[example.qas_id] = best_non_null_entry.text
                 all_nbest_json[example.qas_id] = nbest_json
 
-    with open(output_prediction_file, "w") as writer:
-        writer.write(json.dumps(all_predictions, indent=4) + "\n")
+    with open(output_prediction_file, "w",encoding="utf-8") as writer:
+        writer.write(json.dumps(all_predictions, indent=4,ensure_ascii=False) + "\n")
 
-    with open(output_nbest_file, "w") as writer:
-        writer.write(json.dumps(all_nbest_json, indent=4) + "\n")
+    with open(output_nbest_file, "w",encoding="utf-8") as writer:
+        writer.write(json.dumps(all_nbest_json, indent=4,ensure_ascii=False) + "\n")
 
     if version_2_with_negative:
-        with open(output_null_log_odds_file, "w") as writer:
-            writer.write(json.dumps(scores_diff_json, indent=4) + "\n")
+        with open(output_null_log_odds_file, "w",encoding="utf-8") as writer:
+            writer.write(json.dumps(scores_diff_json, indent=4,ensure_ascii=False) + "\n")
 
 
 def get_final_text(pred_text, orig_text, do_lower_case, verbose_logging=False):
